5bd414d849e5a507b99ab858e473b593ef6db4a5
Harshetamahajan <harshetamahajan@gmail.com>
2023-05-23 12:05:07 +0530

    changes done on 11 line on dev branch

--- Employee.java
+++ Employee.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Employee {
     private int EmpId;
     private String Name;
-    private int Salary;
+    private int Salary;  //changes made on 11 line
 
     public Employee(int empId, String name, int salary) {
         this.EmpId = empId;
